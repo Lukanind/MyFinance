e6439ca76d58e29c631844309f4675b8783724dd
Lukanind <108184134+Lukanind@users.noreply.github.com>
2025-01-13 22:03:45 +0300

    До связывания Operation с Category

--- app/src/main/java/com/example/myfinance/ui/CategoryDropdown.kt
+++ app/src/main/java/com/example/myfinance/ui/CategoryDropdown.kt
@@ -1,36 +1,24 @@
 package com.example.myfinance.ui
 
-import android.util.Log
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ExposedDropdownMenuBox
-import androidx.compose.material3.ExposedDropdownMenuDefaults
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.myfinance.data.CategoryEntity
 import com.example.myfinance.viewmodels.CategoryViewModel
-import com.example.myfinance.viewmodels.OperationViewModel
 
 //@OptIn(ExperimentalMaterial3Api::class)
 //@Composable
